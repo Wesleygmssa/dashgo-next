496ac58cadce4f05640e8b98a5ff110f6f6cc38a
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-03 16:01:09 -0300

    Limpeza de components não utilizados

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,14 @@
-import { Box, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  VStack,
+  Divider,
+  SimpleGrid,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
@@ -8,7 +18,32 @@ export default function CretaeUser() {
       <Header />
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8"></Box>
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Heading size="lg" fontWeight="normal">
+            Criar usuário
+          </Heading>
+          <Divider my="6" borderColor="gray.700" />
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="name" label="Nome completo" />
+              <Input name="email" label="E-mail" type="email" />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="password" type="password" label="senha" />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmação da senha"
+              />
+            </SimpleGrid>
+          </VStack>
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Button colorScheme="pink">Salvar</Button>
+            </HStack>
+          </Flex>
+        </Box>
       </Flex>
     </Box>
   );
