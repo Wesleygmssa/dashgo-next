abf475d61b6782db3cc02a5a74842ee651ffda1a
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-02 22:26:45 -0300

    Criando Dashboard

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
-import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
-import { RiDashboardLine } from "react-icons/ri";
+import { Box, Stack, Link, Icon, Text } from "@chakra-ui/react";
+import { RiContactsLine, RiDashboardLine } from "react-icons/ri";
 
 export function Sidebar() {
   return (
@@ -10,12 +10,36 @@ export function Sidebar() {
             GERAL
           </Text>
           <Stack spacing="4" mt="8" align="stretch">
-            <Link>
-              <Icon as={RiDashboardLine} fontSize="20">
-                <Text ml="4" fontWeight="medium">
-                  Dashboard
-                </Text>
-              </Icon>
+            <Link display="flex" align="center">
+              <Icon as={RiDashboardLine} fontSize="20" />
+              <Text ml="4" fontWeight="medium">
+                Dashboard
+              </Text>
+            </Link>
+            <Link display="flex" align="center">
+              <Icon as={RiContactsLine} fontSize="20" />
+              <Text ml="4" fontWeight="medium">
+                Usuários
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="small">
+            GERAL
+          </Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" align="center">
+              <Icon as={RiDashboardLine} fontSize="20" />
+              <Text ml="4" fontWeight="medium">
+                Dashboard
+              </Text>
+            </Link>
+            <Link display="flex" align="center">
+              <Icon as={RiContactsLine} fontSize="20" />
+              <Text ml="4" fontWeight="medium">
+                Usuários
+              </Text>
             </Link>
           </Stack>
         </Box>
