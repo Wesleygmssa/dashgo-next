d44b7494faf685b987195c7b0f75f57a5833aa3f
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-18 22:02:04 -0300

    Add  axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -19,30 +19,12 @@ import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
 import Link from "next/link";
-import { useQuery } from "react-query";
 import { Spinner } from "@chakra-ui/react";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UsersList() {
   //users => cache
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
-
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
-
-    return users;
-  });
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWindeVersion = useBreakpointValue({
     base: false,
@@ -58,6 +40,9 @@ export default function UsersList() {
           <Flex mb="8" justifyContent="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href="users/create" passHref>
               <Button
---
+++ src/services/hooks/useUsers.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "react-query";
+import { api } from "../api";
+
+export function useUsers() {
+  return useQuery(
+    "users",
+    async () => {
+      const { data } = await api.get("users"); // chamanda api
+
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        };
+      });
+
+      return users;
+    },
+    {
+      staleTime: 1000 * 5,
+    }
+  );
+}
