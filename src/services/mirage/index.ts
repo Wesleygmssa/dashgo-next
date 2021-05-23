c4909f87f47729de0d192448ba4fa4faddc387dc
Wesley Guerra <guerra_ssa@hotmail.com>
2021-05-23 18:10:25 -0300

    Criando lógica de paginação no miraje.js

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from "miragejs";
+import { createServer, Factory, Model, Response } from "miragejs";
 import faker from "faker"; //Dados fakes
 //formatos dos dados
 type User = {
@@ -29,14 +29,32 @@ export function makeServer() {
     },
     //Salvando dados fake no banco de dados
     seeds(server) {
-      server.createList("user", 10);
+      server.createList("user", 200);
     },
     //rotas fake para conexão api
     routes() {
       this.namespace = "api";
       this.timing = 750;
 
-      this.get("/users");
+      this.get("/users", function (schema, request) {
+        //logica de páginação
+
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all("user").length;
+        //10 - 20
+        //exemplo               3 - 1 = 2 * 10 = 20
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        // 20 + 10
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(200, { "x-total-count": String(total) }, { users });
+      });
       this.post("/users");
 
       this.namespace = "";
