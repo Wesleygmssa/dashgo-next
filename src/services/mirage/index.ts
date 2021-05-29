3d1db440d1d940c55cceb3858cfaedd27475fcb5
Wesley Guerra <guerra_ssa@hotmail.com>
2021-05-29 20:31:33 -0300

    update

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -5,57 +5,61 @@ import {
   Response,
   ActiveModelSerializer,
 } from "miragejs";
-import faker from "faker"; //Dados fakes
-//formatos dos dados
+
+import faker from "faker";
+
 type User = {
-  name: String;
-  email: String;
+  name: string;
+  email: string;
   created_at: string;
 };
 
 export function makeServer() {
   const server = createServer({
-    //Como deve interpreta os dados que são enviados por ele
     serializers: {
       application: ActiveModelSerializer,
     },
+
     models: {
       user: Model.extend<Partial<User>>({}),
     },
-    //Dados fakes
+
     factories: {
       user: Factory.extend({
-        name(i: number) {
-          //   return `User ${i + 1}`;
-          return faker.internet.userName();
+        name(i) {
+          return `${faker.name.firstName()} ${faker.name.lastName()}`;
+          // return `User ${i + 1}`;
         },
         email() {
           return faker.internet.email().toLowerCase();
         },
         createdAt() {
-          return faker.date.recent(10, new Date());
+          return faker.date.recent(10);
         },
       }),
     },
-    //Salvando dados fake no banco de dados
+
     seeds(server) {
-      server.createList("user", 200);
+      server.createList("user", 2);
     },
-    //rotas fake para conexão api
+
     routes() {
       this.namespace = "api";
       this.timing = 750;
 
       this.get("/users", function (schema, request) {
-        //logica de páginação
-
-        const { page = 1, per_page = 5 } = request.queryParams;
+        const { page = 1, per_page = 10 } = request.queryParams;
 
         const total = schema.all("user").length;
-        //10 - 20
-        //exemplo               3 - 1 = 2 * 10 = 20
+
+        /*
+         Ex: page = 3, per_page = 15.
+         pageStart = 30 ou 2*15 ou (Number(page) - 1) * Number(per_page);
+         pageEnd = 45 ou 30+15 ou pageStart + Number(per_page);
+         vai do registro 30 ao 45
+        */
+
         const pageStart = (Number(page) - 1) * Number(per_page);
-        // 20 + 10
         const pageEnd = pageStart + Number(per_page);
 
         const users = this.serialize(schema.all("user")).users.slice(
@@ -63,11 +67,11 @@ export function makeServer() {
           pageEnd
         );
 
-        //total de páginas
         return new Response(200, { "x-total-count": String(total) }, { users });
       });
-      this.get("/users/:id"); //lista usuário pelo ID
-      this.post("/users"); // Rota de salvar usuário
+
+      this.get("/users/:id");
+      this.post("/users");
 
       this.namespace = "";
       this.passthrough();
@@ -76,7 +80,3 @@ export function makeServer() {
 
   return server;
 }
-
-/***
- * Necessario configurar e chamar a função no arquivo _app.tsx
- * */
